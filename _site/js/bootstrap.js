a8ebfb52b3d5c47eb59b490d537265ff307ee061
heeraj123 <heeraj123@gmail.com>
2015-09-13 11:58:17 +0530

    Adding new blog

--- _site/js/bootstrap.js
+++ _site/js/bootstrap.js
@@ -8,6 +8,16 @@
   ga('send', 'pageview');
 
 </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-0540814478217300"
+     data-ad-slot="5956699124"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 /*!
  * Bootstrap v3.3.4 (http://getbootstrap.com)
  * Copyright 2011-2015 Twitter, Inc.
