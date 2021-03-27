755653d5d8576d7630c71821992002c628bd736d
Heeraj <h33raj@users.noreply.github.com>
2021-03-27 16:40:10 +0800

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "blog/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "blog/js"
+javascripts_dir = "js"
 fonts_dir = "fonts"
 
 output_style = :expanded
